3a845bfbb492552d39da648c9ecd1a3f220a658e
Eddie Huang <ichihh@uci.edu>
2013-06-12 16:54:35 -0700

    chords?

--- ucimusic147-2/ucimusic147-2/modified/MyScale.h
+++ ucimusic147-2/ucimusic147-2/modified/MyScale.h
@@ -18,6 +18,29 @@ extern int harminScale[8];
 extern int natminScale[8];
 extern int blueScale[8];
 
+typedef enum { Major, Maj2, sus4, min, Maj7, min7, dom7, firstInv } ChordType;
+
+/* 
+ Major
+    0, 7, 16, 19
+ Major2
+    0, 7, 14, 16
+ sus4
+    0, 7, 12, 17
+ min
+    0, 7, 15, 19
+ Maj7
+    0, 7, 11, 16
+ min7
+    0, 7, 10, 15
+ dom7
+    0, 7, 10, 16
+ firstInv
+    4, 12, 19, 24
+ 
+ */
+
+
 extern float freqOf(int noteNumber);
 extern int noteNumberOf(noteType noteName, int octave);
 
